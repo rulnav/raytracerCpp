35e96a2507298498f87b486f53fc5c9eca6c71b5
det <det@melexis.com>
2023-03-28 00:12:37 +0300

    WIP, debugging

--- HomeWork2-normalizing/main.cpp
+++ HomeWork2-normalizing/main.cpp
@@ -6,17 +6,47 @@
 #include "vectorclass.hpp"
 #include "pixelarray.hpp"
 
+#define VECTOR_ARRAY_TYPE std::vector<std::vector<VectorF>>
+
 const uint32_t pixelMatrixHight_g = 400;
 const uint32_t pixelMatrixWidth_g = 600;
 
-void normalizeRays()
+void normalizeRays(std::vector<std::vector<VectorF>>& vectorArray, PixelMatrix& window)
 {
+    uint32_t width = window.getWidth();
+    uint32_t hight = window.getHight();
+    unsigned int multiplier = 255;
 
+    auto normalizeX = [](uint32_t x, uint32_t width) constexpr{
+        return ( 2.0 * ((x+0.5) / width) - 1.0 );
+    };
+
+    auto normalizeY = [](uint32_t y, uint32_t hight) constexpr{
+        return ( 1.0 - ((y+0.5) / hight) * 2.0 );
+    };
+
+//    VectorF trialVec(normalizeX(399, width), normalizeY(383, hight), -1.0);
+//    std::cout<<trialVec.getY();
+    for(uint32_t x = 399; x < width; ++x){
+        for(uint32_t y = 382; y < hight; ++y){
+            VectorF vec(normalizeX(x, width), normalizeY(y, hight), -1.0);
+            std::cout<<'\n'<<"x = "<<x << " calculated value is "<<
+                       vec.getX()<<" ; y = " <<y <<" calculated value is "<<
+                       vec.getY();
+            window.setPixel(x,y,Pixel(vec.getX() * multiplier,
+                                      vec.getY() * multiplier,
+                                      0x77));
+            vectorArray[x][y] = vec;
+        }
+    }
 }
 
 int main()
 {
+    std::vector<std::vector<VectorF>> vectorArray
+            (pixelMatrixHight_g, std::vector<VectorF>(pixelMatrixWidth_g));
 	PixelMatrix window(pixelMatrixWidth_g, pixelMatrixHight_g);
-    window.fillPixelMatrix(0xff0000);
+//    window.fillPixelMatrix(0xff0000);
+    normalizeRays(vectorArray, window);
     return window.fillPpmFile("./vectornormalization.ppm");
 }
--- HomeWork2-normalizing/pixelarray.hpp
+++ HomeWork2-normalizing/pixelarray.hpp
@@ -7,106 +7,106 @@
 #include <vector>
 
 namespace MyUtils {
-  template<typename T>
-  T diff(const T&a, const T&b) {
+template<typename T>
+T diff(const T&a, const T&b) {
     return (a > b) ? (a - b) : (b - a);
-  }
+}
 }
 
 class Pixel {
 public:
-  uint8_t red, green, blue;  
+    uint8_t red, green, blue;
 
-  Pixel() : red{0}, green {0}, blue{0} {}
-  Pixel(const uint8_t inRed, const uint8_t inGreen, const uint8_t inBlue) :
-    red{inRed}, green{inGreen}, blue{inBlue} {}
-  Pixel(const uint32_t inColor) {
-    red =   (inColor>>8*2)&0xFF;
-    green = (inColor>>8*1)&0xFF;
-    blue =  (inColor>>8*0)&0xFF;
-  }
+    Pixel() : red{0}, green {0}, blue{0} {}
+    Pixel(const uint8_t inRed, const uint8_t inGreen, const uint8_t inBlue) :
+        red{inRed}, green{inGreen}, blue{inBlue} {}
+    Pixel(const uint32_t inColor) {
+        red =   (inColor>>8*2)&0xFF;
+        green = (inColor>>8*1)&0xFF;
+        blue =  (inColor>>8*0)&0xFF;
+    }
 
-  friend std::ostream& operator<<(std::ostream& os, const Pixel& c) {
-    os << static_cast<int>(c.red) << ' '
-       << static_cast<int>(c.green) << ' '
-       << static_cast<int>(c.blue);
-    return os;
-  }
+    friend std::ostream& operator<<(std::ostream& os, const Pixel& c) {
+        os << static_cast<int>(c.red) << ' '
+           << static_cast<int>(c.green) << ' '
+           << static_cast<int>(c.blue);
+        return os;
+    }
 };
 
 class PixelMatrix {
 public:
-  PixelMatrix(const uint32_t hight, const uint32_t width)
-  : pixelMatrixArray(width, std::vector<Pixel>(hight)) {}
+    PixelMatrix(const uint32_t hight, const uint32_t width)
+        : pixelMatrixArray(width, std::vector<Pixel>(hight)) {}
 
-  uint32_t getHight() const { return pixelMatrixArray.size(); }
-  uint32_t getWidth() const { return pixelMatrixArray.empty() ? 0 : pixelMatrixArray[0].size(); }
+    uint32_t getHight() const { return pixelMatrixArray.size(); }
+    uint32_t getWidth() const { return pixelMatrixArray.empty() ? 0 : pixelMatrixArray[0].size(); }
 
-  Pixel getPixel(const uint32_t x, const uint32_t y) const {
-    return pixelMatrixArray[x][y];
-  }
-
-  void setPixel(const uint32_t x, const uint32_t y, const Pixel &color) {
-    pixelMatrixArray[x][y] = color;
-  }
-
-  bool fillPpmFile(std::string file_path) const {
-    std::ofstream ppm_file;
-    ppm_file.open(file_path);
-    if(!ppm_file.is_open()) return false;
-    ppm_file<<"P3\n";
-    ppm_file<<getWidth()<<" "<<getHight()<<'\n';
-    ppm_file<<0xFF<<'\n';
-    for(auto& col : pixelMatrixArray){
-      for(auto& pixel : col){
-        ppm_file<<pixel<<' ';
-      }
-      ppm_file << '\n';
+    Pixel getPixel(const uint32_t x, const uint32_t y) const {
+        return pixelMatrixArray[x][y];
     }
-    ppm_file.close();
-    return true;
-  }
 
-  void fillPixelMatrix(const Pixel &color){
-    for(auto& col : pixelMatrixArray){
-      for(auto& pixel : col){
-        pixel = color;
-      }
+    void setPixel(const uint32_t x, const uint32_t y, const Pixel &color) {
+        pixelMatrixArray[x][y] = color;
     }
-  }
 
-  void fillRectangle(const uint32_t rx1, const uint32_t ry1, const uint32_t rw, const uint32_t rh, const Pixel &color) {
-    uint32_t rx2 = rx1+rw;
-    uint32_t ry2 = ry1+rh;
-    if( (ry2> getHight()) || (rx2 > getWidth()) ) return;
-    for(uint32_t y = ry1; y < ry2; ++y){
-      for(uint32_t x = rx1; x < rx2; ++x){
-        setPixel(x, y, color);
-      }
-    }
-  }
-
-  void fillCircle(const uint32_t cx, const uint32_t cy, const uint32_t cr, const Pixel &color) {
-    uint32_t x1 = (cr < cx) ? cx - cr : 0;
-    uint32_t x2 = cx + cr;
-    uint32_t y1 = (cr < cy) ? cy - cr : 0;
-    uint32_t y2 = cy + cr; 
-    uint32_t dx, dy;
-    for(uint32_t xi = x1; xi <= x2; ++xi){
-      if (xi >= getWidth()) break;
-      dx = MyUtils::diff(cx, xi);
-      for(uint32_t yi = y1; yi <= y2; ++yi){
-        if (yi >= getHight()) break;
-        dy = MyUtils::diff(cy, yi);
-        if( dx*dx + dy*dy <= cr*cr){
-          setPixel(xi, yi, color);
+    bool fillPpmFile(std::string file_path) const {
+        std::ofstream ppm_file;
+        ppm_file.open(file_path);
+        if(!ppm_file.is_open()) return false;
+        ppm_file<<"P3\n";
+        ppm_file<<getWidth()<<" "<<getHight()<<'\n';
+        ppm_file<<0xFF<<'\n';
+        for(auto& col : pixelMatrixArray){
+            for(auto& pixel : col){
+                ppm_file<<pixel<<' ';
+            }
+            ppm_file << '\n';
         }
-      }
+        ppm_file.close();
+        return true;
     }
-  }
+
+    void fillPixelMatrix(const Pixel &color){
+        for(auto& col : pixelMatrixArray){
+            for(auto& pixel : col){
+                pixel = color;
+            }
+        }
+    }
+
+    void fillRectangle(const uint32_t rx1, const uint32_t ry1, const uint32_t rw, const uint32_t rh, const Pixel &color) {
+        uint32_t rx2 = rx1+rw;
+        uint32_t ry2 = ry1+rh;
+        if( (ry2> getHight()) || (rx2 > getWidth()) ) return;
+        for(uint32_t y = ry1; y < ry2; ++y){
+            for(uint32_t x = rx1; x < rx2; ++x){
+                setPixel(x, y, color);
+            }
+        }
+    }
+
+    void fillCircle(const uint32_t cx, const uint32_t cy, const uint32_t cr, const Pixel &color) {
+        uint32_t x1 = (cr < cx) ? cx - cr : 0;
+        uint32_t x2 = cx + cr;
+        uint32_t y1 = (cr < cy) ? cy - cr : 0;
+        uint32_t y2 = cy + cr;
+        uint32_t dx, dy;
+        for(uint32_t xi = x1; xi <= x2; ++xi){
+            if (xi >= getWidth()) break;
+            dx = MyUtils::diff(cx, xi);
+            for(uint32_t yi = y1; yi <= y2; ++yi){
+                if (yi >= getHight()) break;
+                dy = MyUtils::diff(cy, yi);
+                if( dx*dx + dy*dy <= cr*cr){
+                    setPixel(xi, yi, color);
+                }
+            }
+        }
+    }
 
 private:
-  std::vector<std::vector<Pixel>> pixelMatrixArray;
+    std::vector<std::vector<Pixel>> pixelMatrixArray;
 };
 
 #endif
